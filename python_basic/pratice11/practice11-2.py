1f9eccccac1f6e38a2571f447746f3767d469ef5
khyunsuuu <dkrl0256@gmail.com>
2023-07-25 19:44:19 +0900

    패키지 사용(__all__)

--- python_basic/pratice11/practice11-2.py
+++ python_basic/pratice11/practice11-2.py
@@ -4,7 +4,6 @@
 import travel.thailand # 모듈, 패키지만 import 가능
 trip_to = travel.thailand.ThailandPackage()
 trip_to.detail()
-'''
 
 from travel.thailand import ThailandPackage
 trip_to = ThailandPackage()
@@ -12,4 +11,9 @@ trip_to.detail()
 
 from travel import vietnam
 trip_to = vietnam.VietnamPackage()
+trip_to.detail()
+'''
+from travel import *
+#trip_to = vietnam.VietnamPackage()
+trip_to = thailand.ThailandPackage()
 trip_to.detail()
